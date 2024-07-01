25a7ac40760f54112ce346f54addc10eaaa9e488
VesperSpoony <714694113@qq.com>
2024-07-01 13:55:36 +0800

    day3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react"
+import { useSelector, useDispatch } from "react-redux"
+import { increment, decrement, addToNum } from "./store/modules/counterStore"
+import { fetchChannelList } from "./store/modules/channelStore"
 
 function App() {
+  const { count } = useSelector(state => state.counter)
+  const { channelList } = useSelector(state => state.channel)
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(fetchChannelList())
+  }, [dispatch])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => dispatch(decrement())}>-</button>
+      {count}
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(addToNum(10))}>+10</button>
+      <button onClick={() => dispatch(addToNum(20))}>+20</button>
+      <ul>
+        {channelList.map(item => <li key={item.id}>{item.name}</li>)}
+      </ul>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
